7778a7c1f4065b71d8eb40c6175aabc6756ec9e4
Ryan Ju <r.ju2014@gmail.com>
2021-03-21 19:45:39 +0000

    Fixed links (#2)
    Co-authored-by: Ran Ju <ryan@trinnylondon.com>

---
+++ _next/static/W0BoLHMlq7hJjSwvyEUOL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-eda2a39bbd06f273327a.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-984ebf722813ef90af09.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
